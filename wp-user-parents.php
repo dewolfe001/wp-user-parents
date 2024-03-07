76f719509e38f69c89a9a34d8caca36ed549fb17
Shawn DeWolfe <shawn@shawndewolfe.com>
2024-03-06 22:18:42 -0800

    Update wp-user-parents.php
    Updating code

--- wp-user-parents.php
+++ wp-user-parents.php
@@ -2,13 +2,13 @@
 
 /*
  * Plugin Name: WP User Parents
- * Plugin URI:  http://wordpress.org/plugins/wp-user-parents/
- * Author:      John James Jacoby
- * Author URI:  https://profiles.wordpress.org/johnjamesjacoby/
+ * Plugin URI:  https://web321.co/plugins/wp-user-parents/
+ * Author:      dewolfe001
+ * Author URI:  https://profiles.wordpress.org/dewolfe001/
  * License:     GPLv2 or later
  * License URI: https://www.gnu.org/licenses/gpl-2.0.html
- * Description: Allow parent users to manage their children
- * Version:     0.1.0
+ * Description: Allow parent users to manage their child accounts
+ * Version:     1.0.0
  * Text Domain: 
  * Domain Path: /wp-user-parents/assets/languages/
  */
@@ -16,23 +16,24 @@
 // Exit if accessed directly
 defined( 'ABSPATH' ) || exit;
 
+define('WUPM_NAME', plugin_basename(__FILE__));
+define('WUPM_DIR', plugin_dir_path(__FILE__));
+define('WUPM_URL', plugin_dir_url(__FILE__));
+define('WUPM_VERSION', '1.0.0');
+
 /**
  * Enqueue assets
  *
  * @since 0.1.0
  */
 function _wp_user_parents() {
-
-	// Get the plugin path
-	$plugin_path = plugin_dir_path( __FILE__ ). 'wp-user-parents/';
-
 	// Required Files
-	require_once $plugin_path . 'includes/admin.php';
-	require_once $plugin_path . 'includes/capabilities.php';
-	require_once $plugin_path . 'includes/functions.php';
-	require_once $plugin_path . 'includes/metaboxes.php';
-	require_once $plugin_path . 'includes/user-dashboard.php';
-	require_once $plugin_path . 'includes/hooks.php';
+	require_once WUPM_DIR . 'includes/admin.php';
+	require_once WUPM_DIR . 'includes/capabilities.php';
+	require_once WUPM_DIR . 'includes/functions.php';
+	require_once WUPM_DIR . 'includes/metaboxes.php';
+	require_once WUPM_DIR . 'includes/user-dashboard.php';
+	require_once WUPM_DIR . 'includes/hooks.php';
 }
 add_action( 'plugins_loaded', '_wp_user_parents' );
 
@@ -44,7 +45,7 @@ add_action( 'plugins_loaded', '_wp_user_parents' );
  * @return string
  */
 function wp_user_parents_get_plugin_url() {
-	return plugin_dir_url( __FILE__ ) . 'wp-user-parents/';
+	return WUPM_DIR;
 }
 
 /**
@@ -55,5 +56,5 @@ function wp_user_parents_get_plugin_url() {
  * @return int
  */
 function wp_user_parents_get_asset_version() {
-	return 201601060001;
+	return 202401060001;
 }
